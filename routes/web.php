8f21f01d22ea84f1c4c1a2d1d9113c637b7e8199
unknown <raudhiatul@gmail.com>
2021-10-18 11:13:23 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Models\Category;
+use App\Models\User;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -38,9 +39,15 @@ Route::get('/categories', function () {
 });
 
 Route::get('/categories/{category:slug}', function(Category $category){
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
+        'posts' => $category->posts->load('category', 'author')
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });
